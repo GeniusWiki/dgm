e8f3ad4a8759ea61ecb785072817d03ae70c62e1
Dapeng Ni <dapeng.ni@buildingiq.com>
2012-08-03 18:04:01 +1000

    new options

--- dgm/main.py
+++ dgm/main.py
@@ -34,18 +34,29 @@ def _init(dgm):
         print ("Home directory is not valid directory")
         exit(1)
     
-    #already mkdir default metadata directory
+    #Mkdir for this server
+    server_path = os.path.join(home_path, dgm.args.s)
+    if not os.path.exists(server_path):
+        os.mkdir(server_path)
+    
+    #Also mkdir default metadata directory
     meta_path = os.path.join(home_path, "__metadata")
     if not os.path.exists(meta_path):
         os.mkdir(meta_path)
     
+    
+    #init git
     dgm.home_path = home_path
     _run_cmd_from_home(dgm, "git init")
     
+    if dgm.args.r:
+        _run_cmd_from_home(dgm, "git remote add origin %s" % dgm.args.r)
+        
     confParser = ConfigParser.ConfigParser()
     confParser.add_section("config")
     confParser.set("config", "server_name", dgm.args.s)
     confParser.set("config", "home_path", home_path)
+    confParser.set("config", "git_url", dgm.args.r)
     
     conf_file = open(conf, 'w')
     confParser.write(conf_file)
@@ -54,7 +65,7 @@ def _init(dgm):
     print("Initialise dgm repository successfully.")
     
 def _add(dgm):
-    """Copy file to repository with path. """
+    """Copy file with original path to server path under current local repository . """
     files = dgm.args.filename
     
     for src_file in files:
@@ -70,7 +81,7 @@ def _add(dgm):
             src_file_path = os.path.dirname(src_file)
             src_file_name = os.path.basename(src_file)
             
-            tgt_file_path = os.path.join(dgm.home_path, src_file_path.lstrip(os.sep))
+            tgt_file_path = os.path.join(dgm.server_path, src_file_path.lstrip(os.sep))
             if not os.path.exists(tgt_file_path):
                 os.makedirs(tgt_file_path, 0700)
                 
@@ -89,12 +100,19 @@ def _status(dgm):
     """ Retrieve local repository status"""
     _run_cmd_from_home(dgm, "git status")
 
-def _run_cmd_from_home(dgm, cmd):
-    pwd = os.getcwd()
-    os.chdir(dgm.home_path)
-    os.system(cmd)
-    os.chdir(pwd)
-    
+def _push(dgm):
+    """ Push local repository master to remote """
+    if dgm.git_url:
+        _run_cmd_from_home(dgm, "git push origin master")
+    else:
+        print "No valid remote git URL set. Run [dgm remote -r {gitURL}] first"
+        exit(1)
+
+def _remote(dgm):
+    """ Add remote repository as origin """
+    _run_cmd_from_home(dgm, "git remote add origin %s", dgm.args.r)
+
+
 def main():
     dgm = DGM()
     
@@ -106,6 +124,25 @@ def main():
         _status(dgm)
     elif dgm.args.command == 'commit':
         _commit(dgm)
+    elif dgm.args.command == 'push':
+        _push(dgm)
+    elif dgm.args.command == 'remote':
+        _remote(dgm)
+    elif dgm.args.command == 'print':
+        print ("======================================================")
+        print ("Server name: [%s]" % dgm.server_name)
+        print ("Home directory: [%s]" % dgm.home_path)
+        print ("Home for server directory: [%s]" % dgm.server_path)
+        print ("Git URL: [%s]" % dgm.git_url)
+        print ("======================================================")
+        
+
+def _run_cmd_from_home(dgm, cmd):
+    pwd = os.getcwd()
+    os.chdir(dgm.home_path)
+    os.system(cmd)
+    os.chdir(pwd)
+    
         
 class DGM:
     def __init__(self):
@@ -118,6 +155,7 @@ class DGM:
         cmd_init_parser = subparsers.add_parser("init", help="Init local repository")
         cmd_init_parser.add_argument("-s", help="server name", required=True)
         cmd_init_parser.add_argument("-l", help="home directory - absolute path or start with tilde. If not preset, using current directory.")
+        cmd_init_parser.add_argument("-r", help="remote git url")
         
         #Add
         cmd_add_parser = subparsers.add_parser("add", help="Add file to local repository")
@@ -128,7 +166,18 @@ class DGM:
         cmd_commit_parser.add_argument('-m', help='comment', required=True)
         
         #Status
-        cmd_status_parser = subparsers.add_parser("status", help="view status of local repository")
+        subparsers.add_parser("status", help="Check status of local repository")
+        
+        #Push
+        subparsers.add_parser("push", help="Push local repository master to remote")
+        
+        
+        #remote
+        cmd_commit_parser = subparsers.add_parser("remote", help="Remote git management ")
+        cmd_commit_parser.add_argument('-r', help='Remote git URL', required=True)
+        
+        #Print configuration
+        subparsers.add_parser("print", help="Print configuration options")
         
         self.args = parser.parse_args()
         
@@ -139,10 +188,18 @@ class DGM:
             confParser.read(conf_file)
             self.server_name = confParser.get('config', 'server_name');
             self.home_path = confParser.get('config', 'home_path')
+            try:
+                self.git_url = confParser.get('config', 'git_url')
+            except ConfigParser.NoOptionError:
+                self.git_url = None
+            
+            #other variables
             self.meta_path = os.path.join(self.home_path, "__metadata")
+            self.server_path = os.path.join(self.home_path, self.server_name)
         else:
             if self.args.command != 'init':
                 print "Run init command first"
+
     
 if __name__ == "__main__":
     main()
---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo ./setup.py install
+sudo rm -rf build/  dgm.egg-info/  dist/
