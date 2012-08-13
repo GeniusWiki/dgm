c9ed671ae7365059bd756ccbc3c9707edd0c65cc
Dapeng Ni <dapeng.ni@buildingiq.com>
2012-08-13 14:44:17 +1000

    readme, pull and apply

--- dgm/main.py
+++ dgm/main.py
@@ -35,7 +35,7 @@ def _init(dgm):
         exit(1)
     
     #Mkdir for this server
-    server_path = os.path.join(home_path, dgm.args.s)
+    server_path = os.path.join(home_path, dgm.args.n)
     if not os.path.exists(server_path):
         os.mkdir(server_path)
     
@@ -49,14 +49,14 @@ def _init(dgm):
     dgm.home_path = home_path
     _run_cmd_from_home(dgm, "git init")
     
-    if dgm.args.r:
-        _run_cmd_from_home(dgm, "git remote add origin %s" % dgm.args.r)
+    if dgm.args.s:
+        _run_cmd_from_home(dgm, "git remote add origin %s" % dgm.args.s)
         
     confParser = ConfigParser.ConfigParser()
     confParser.add_section("config")
     confParser.set("config", "server_name", dgm.args.s)
     confParser.set("config", "home_path", home_path)
-    confParser.set("config", "git_url", dgm.args.r)
+    confParser.set("config", "git_url", dgm.args.s)
     
     conf_file = open(conf, 'w')
     confParser.write(conf_file)
@@ -84,15 +84,47 @@ def _add(dgm):
             
             tgt_file_path = os.path.join(dgm.server_path, src_file_path.lstrip(os.sep))
             if not os.path.exists(tgt_file_path):
+                #TODO: permission need copy from origin
                 os.makedirs(tgt_file_path, 0700)
                 
             tgt_file = os.path.join(tgt_file_path, src_file_name)
             shutil.copy(src_file, tgt_file)
             
             _run_cmd_from_home(dgm, "git add %s" % tgt_file)
+            
+            if dgm.args.init:
+                _run_cmd_from_home(dgm, "git commit -m'Initial - %s'" % src_file)
+                
             _stdout_info("%s is added to DGM repository" % src_file)
         
+def _apply(dgm):
+    """Copy repository file to overwrite managed file"""
+    files = dgm.args.filename
+    for src_file in files:
+        if not src_file.startswith(os.sep):
+            src_file = os.path.join(os.getcwd(), src_file)
+            
+        src_file_path = os.path.dirname(src_file)
+        src_file_name = os.path.basename(src_file)
+        
+        tgt_file_path = os.path.join(dgm.server_path, src_file_path.lstrip(os.sep))
+        tgt_file = os.path.join(tgt_file_path, src_file_name)
+        if not os.path.exists(tgt_file):
+            _stdout_error("File %s does not exist in DGM repository" % tgt_file)
+            exit(1)
 
+        if os.path.isfile(tgt_file):
+            if not os.path.exists(src_file_path):
+                #TODO: permission need copy from origin
+                os.makedirs(src_file_path, 0700)
+            
+            shutil.copy(tgt_file, src_file)
+            _stdout_info("%s is applied" % tgt_file)
+        else:
+            _stdout_error("DGM File %s is not file" % tgt_file)
+            
+            
+                
 def _update(dgm):
     """ Refresh all dgm with original files"""
     _compare_files(dgm, True)
@@ -103,15 +135,15 @@ def _commit(dgm):
     
 def _status(dgm):
     """ Retrieve local repository status"""
-    _stdout_error("DGM repository status")
-    print ("=========================================")
+    _stdout("DGM repository status")
+    _stdout("=========================================")
     dirty = _compare_files(dgm)
     if not dirty:
         _stdout_info("DGM managed files all synchronised with repository.")
     
-    print ("")
-    _stdout_error("DGM git status")
-    print("=========================================")
+    _stdout ("")
+    _stdout("DGM git status")
+    _stdout("=========================================")
     _run_cmd_from_home(dgm, "git status")
 
 def _push(dgm):
@@ -121,22 +153,39 @@ def _push(dgm):
     else:
         _stdout_error("No valid remote git URL set. Run [dgm remote -r {gitURL}] first")
         exit(1)
+        
+def _pull(dgm):
+    """ Pull remote to  local repository master """
+    if dgm.git_url:
+        _run_cmd_from_home(dgm, "git pull origin master")
+    else:
+        _stdout_error("No valid remote git URL set. Run [dgm remote -r {gitURL}] first")
+        exit(1)
 
 def _remote(dgm):
     """ Add remote repository as origin """
-    _run_cmd_from_home(dgm, "git remote add origin %s" % dgm.args.r)
+    confParser = ConfigParser.ConfigParser()
+    confParser.set("config", "git_url", dgm.args.s)
+    
+    conf_dir = os.path.expanduser("~/.dgm")
+    conf = os.path.join(conf_dir, 'config')
+    conf_file = open(conf, 'w')
+    confParser.write(conf_file)
+    conf_file.close()
+    
+    _run_cmd_from_home(dgm, "git remote add origin %s" % dgm.args.s)
 
 
 def _print(dgm):
-    _stdout_error("Server configration")
-    print("======================================================")
+    _stdout("Server configration")
+    _stdout("======================================================")
     _stdout_info ("Server name: [%s]" % dgm.server_name)
     _stdout_info ("Home directory: [%s]" % dgm.home_path)
     _stdout_info ("Home for server directory: [%s]" % dgm.server_path)
     _stdout_info ("Git URL: [%s]" % dgm.git_url)
     
-    _stdout_error("DGM repository information")
-    print ("======================================================")
+    _stdout("DGM repository information")
+    _stdout ("======================================================")
     
     file_count = 0
     for dgm_file, src_file in _retrieve_files(dgm):
@@ -162,6 +211,10 @@ def main():
         _update(dgm)
     elif dgm.args.command == 'remote':
         _remote(dgm)
+    elif dgm.args.command == 'pull':
+        _pull(dgm)
+    elif dgm.args.command == 'apply':
+        _apply(dgm)
     elif dgm.args.command == 'print':
         _print(dgm)
         
@@ -225,10 +278,15 @@ def _get_src_file(dgm, dgm_file):
         
     return src_file
 
+def _stdout(string):
+    print(string)
+    
 def _stdout_error(string):
+    #red message
     print(_color_message(string, error=True))
     
 def _stdout_info(string):
+    #green message
     print(_color_message(string, error=False))
     
     
@@ -256,12 +314,13 @@ class DGM:
         
         #Init
         cmd_init_parser = subparsers.add_parser("init", help="Init local repository")
-        cmd_init_parser.add_argument("-s", help="server name", required=True)
-        cmd_init_parser.add_argument("-d", help="home directory - absolute path or start with tilde. If not preset, using current directory.")
-        cmd_init_parser.add_argument("-r", help="remote git url")
+        cmd_init_parser.add_argument("-n", help="Server name", required=True)
+        cmd_init_parser.add_argument("-d", help="Home directory - absolute path or start with tilde. If not preset, using current directory.")
+        cmd_init_parser.add_argument("-s", help="Remote git  server url")
         
         #Add
         cmd_add_parser = subparsers.add_parser("add", help="Add file to local repository")
+        cmd_add_parser.add_argument("-init", help="First add this file, then commit immediately after add.", required=False, action='store_const', const=True)
         cmd_add_parser.add_argument("filename", nargs='+')
         
         
@@ -278,10 +337,17 @@ class DGM:
         #Push
         subparsers.add_parser("push", help="Push local repository master to remote")
         
+        #Pull
+        subparsers.add_parser("pull", help="Pull remote repository to local master")
         
-        #remote
+        #Apply
+        cmd_apply_parser = subparsers.add_parser("apply", help="Copy DGM local repository file to overwrite the managed file")
+        cmd_apply_parser.add_argument("filename", nargs='+')
+        
+        
+        #Remote
         cmd_commit_parser = subparsers.add_parser("remote", help="Remote git management ")
-        cmd_commit_parser.add_argument('-r', help='Remote git URL', required=True)
+        cmd_commit_parser.add_argument('-s', help='Remote git URL', required=True)
         
         #Print configuration
         subparsers.add_parser("print", help="Print configuration options")
